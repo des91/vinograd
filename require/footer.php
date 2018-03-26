39d3705280712acb4b0dac06ee41eb266cc1da30
des <web@evenlight.team>
2018-03-26 21:23:18 +0800

    add content

---
+++ require/footer.php
@@ -0,0 +1,45 @@
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#">Главная</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#about">Каталог</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#services">Авторы сортов винограда</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#contact">Вредители и болезни</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#contact">Галерея</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#contact">Контакты</a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted small">&copy; Виноградник Владислава <br>2018</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="content/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="content/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
